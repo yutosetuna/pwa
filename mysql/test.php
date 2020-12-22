62b470f012302c7fc2584e434577ef201fcdbc77
馬場悠登 <babayuto@babayuutounoMacBook-Air.local>
2020-12-22 14:31:20 +0900

    変

--- mysql/test.php
+++ mysql/test.php
@@ -3,8 +3,8 @@
 <body>
 <?php
 echo $_POST["name"]."さんの出身は";
-echo $_POST["from"]."で";
-echo $_POST["sex"]."です。";
+echo $_POST["from"]."で、性別は";
+echo $_POST["sex"]."です。好きな色は";
 echo $_POST["color"];
 ?>
 </form>
