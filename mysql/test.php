1d0fab8254b19a8ceb44ad546429feae33ff0e6f
馬場悠登 <babayuto@babayuutounoMacBook-Air.local>
2020-12-22 14:06:25 +0900

    hennkou

--- mysql/test.php
+++ mysql/test.php
@@ -1,6 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
 <?php
-$name=$_post["name"];
-$from=$_post["from"];
-echo ('<p>投稿者：'.$name.'<p>');
-print('<p>出身：'.$from.'<p>');
+echo $_post["name"];
+echo $_post["from"];
 ?>
+</form>
+</body>
+</html>
