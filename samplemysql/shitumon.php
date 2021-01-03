3104d22988109e8f7c70e05a9afde26e71fc81b3
馬場悠登 <babayuto@babayuutounoMacBook-Air.local>
2021-01-03 21:40:45 +0900

    質問

--- samplemysql/shitumon.php
+++ samplemysql/shitumon.php
@@ -52,7 +52,7 @@ $gakusei = $data->fetchAll();
             </li>
         <?php endforeach; ?>
         </ul>
-        <form action="situmonn.php" method="POST">
+        <form action="situmon.php" method="POST">
         <p>タイトル
 　　　　<input type="search" name="タイトル" value=""><br>
         </p>
